25fcd3f1da3f8442605c6f5b9a0d5fed7df35a82
Vladimir Zapryanov <90334985+VladimirZapryanov@users.noreply.github.com>
2024-06-20 12:54:33 +0300

    Create learn_everything_ai.py

---
+++ learn_everything_ai.py
@@ -0,0 +1,40 @@
+a = 2
+b = 3
+print(a + b)
+
+print((2+4+6)*3-12/3)
+print(8**2)
+
+print((2.0+4.0+6.0)*3.0-12.0/3.0)
+print(8.0**2)
+
+print(7.0/4.0)
+print(7/4)
+
+print(34+1.0)
+
+print(float(1))
+print(int(3.678))
+
+x = 3
+y = 2
+z = 7
+a = x*y*z
+print(a)
+
+print("spam" + " " + "eggs and spam")
+print("spam "*4 + "beans and spam")
+
+a = 1
+b = 1.0
+c = "1.0"
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(2**3)
+print(pow(2, 3))
+
+a = [0, 1, 2]
+a.append(3)
+print(a)
