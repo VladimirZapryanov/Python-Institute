5a545c5ba0de947c886c8ea84d632a1e985e7584
Vladimir Zapryanov <90334985+VladimirZapryanov@users.noreply.github.com>
2024-07-02 17:27:40 +0300

    add more exercise

--- pcep_certification_course.py
+++ pcep_certification_course.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 print('Hello, World!')
 
 print('Hello, Python!')
@@ -266,3 +268,158 @@ print(var != 0)
 #     year_is = 'Leap year'
 # print(year_is)
 
+# # Store the current largest number here.
+# largest_number = -999999999
+#
+# # Input the first value.
+# number = int(input("Enter a number or type -1 to stop: "))
+#
+# # If the number is not equal to -1, continue.
+# while number != -1:
+#     # Is number larger than largest_number?
+#     if number > largest_number:
+#         # Yes, update largest_number.
+#         largest_number = number
+#     # Input the next number.
+#     number = int(input("Enter a number or type -1 to stop: "))
+#
+# # Print the largest number.
+# print("The largest number is:", largest_number)
+
+# # A program that reads a sequence of numbers
+# # and counts how many numbers are even and how many are odd.
+# # The program terminates when zero is entered.
+#
+# odd_numbers = 0
+# even_numbers = 0
+#
+# # Read the first number.
+# number = int(input("Enter a number or type 0 to stop: "))
+#
+# # 0 terminates execution.
+# while number != 0:
+#     # Check if the number is odd.
+#     if number % 2 == 1:
+#         # Increase the odd_numbers counter.
+#         odd_numbers += 1
+#     else:
+#         # Increase the even_numbers counter.
+#         even_numbers += 1
+#     # Read the next number.
+#     number = int(input("Enter a number or type 0 to stop: "))
+#
+# # Print results.
+# print("Odd numbers count:", odd_numbers)
+# print("Even numbers count:", even_numbers)
+
+# secret_number = 777
+#
+# print(
+# """
+# +================================+
+# | Welcome to my game, muggle!    |
+# | Enter an integer number        |
+# | and guess what number I've     |
+# | picked for you.                |
+# | So, what is the secret number? |
+# +================================+
+# """)
+#
+# user_number = int(input())
+# while not user_number == secret_number:
+#     user_number = int(input("Ha ha! You're stuck in my loop!"))
+# print(secret_number)
+# print('Well done, muggle! You are free now.')
+
+# for i in range(2, 8, 3):
+#     print("The value of i is currently", i)
+#
+# for n in range(1, 6):
+#     print(f'{n} Mississippi')
+#     sleep(1)
+#
+# print('Ready or not, here I come!')
+
+# break - example
+print("The break instruction:")
+for i in range(1, 6):
+    if i == 3:
+        break
+    print("Inside the loop.", i)
+print("Outside the loop.")
+
+# continue - example
+print("\nThe continue instruction:")
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print("Inside the loop.", i)
+print("Outside the loop.")
+
+# largest_number = -99999999
+# counter = 0
+# while True:
+#     number = int(input("Enter a number or type -1 to end program: "))
+#     if number == -1:
+#         break
+#     counter += 1
+#     if number > largest_number:
+#         largest_number = number
+# if counter != 0:
+#     print("The largest number is", largest_number)
+# else:
+#     print("You haven't entered any number.")
+#
+# largest_number = -99999999
+# counter = 0
+# number = int(input("Enter a number or type -1 to end program: "))
+# while number != -1:
+#     if number == -1:
+#         continue
+#     counter += 1
+#     if number > largest_number:
+#         largest_number = number
+#     number = int(input("Enter a number or type -1 to end program: "))
+# if counter:
+#     print("The largest number is", largest_number)
+# else:
+#     print("You haven't entered any number.")
+
+# word = input('Enter the word or chupacabra to exit: ')
+# while True:
+#     if word == 'chupacabra':
+#         break
+#     word = input('Enter the word or chupacabra to exit: ')
+# print("You've successfully left the loop.")
+
+# user_word = input('Enter a word: ').upper()
+# for l in user_word:
+#     if l == 'A' or l == 'E' or l == 'I' or l == 'O' or l == 'U':
+#         continue
+#     print(l)
+
+# user_word = input('Enter a word: ').upper()
+# word_without_vowels = ''
+# for l in user_word:
+#     if l == 'A' or l == 'E' or l == 'I' or l == 'O' or l == 'U':
+#         continue
+#     word_without_vowels += l
+# print(word_without_vowels)
+
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("else:", i)
+
+i = 111
+for i in range(2, 1):
+    print(i)
+else:
+    print("else:", i)
+
+for i in range(5):
+    print(i)
+else:
+    print("else:", i)
