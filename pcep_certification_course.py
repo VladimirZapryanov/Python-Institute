df69dd0afc3fccf39f3d7ac2eb2fd3084cf4e84f
Vladimir Zapryanov <90334985+VladimirZapryanov@users.noreply.github.com>
2024-07-03 16:21:02 +0300

    add new exercise

--- pcep_certification_course.py
+++ pcep_certification_course.py
@@ -1,154 +1,154 @@
 from time import sleep
 
-print('Hello, World!')
-
-print('Hello, Python!')
-print('Vladimir')
-
-print("The itsy bitsy spider climbed up the waterspout.")
-print("Down came the rain and washed the spider out.")
-
-print("The itsy bitsy spider climbed up the waterspout.")
-print()
-print("Down came the rain and washed the spider out.")
-
-print("The itsy bitsy spider\nclimbed up the waterspout.")
-print()
-print("Down came the rain\nand washed the spider out.")
-
-print('\\')
-
-print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
-
-print("My name is", "Python.")
-print("Monty Python.")
-
-print("My name is", "Python.", end=" ")
-print("Monty Python.")
-
-print("My name is ", end="")
-print("Monty Python.")
-
-print("My", "name", "is", "Monty", "Python.", sep="-")
-
-print("My", "name", "is", sep="_", end="*")
-print("Monty", "Python.", sep="*", end="*\n")
-
-print("Programming","Essentials","in", sep="***", end="...")
-print("Python")
-
-print("2")
-print(2)
-
-print(0o123)
-print(0x123)
-
-print('I\'m Monty Python.')
-print("I'm Monty Python.")
-
-print(True > False)
-print(True < False)
-
-print('"I\'m"\n""learning""\n"""Python"""')
-
-print(2+2)
-print(2 * 3)
-print(2 * 3.)
-print(2. * 3)
-print(2. * 3.)
-
-print(6 / 3)
-print(6 / 3.)
-print(6. / 3)
-print(6. / 3.)
-
-print(6 // 3)
-print(6 // 3.)
-print(6. // 3)
-print(6. // 3.)
-
-print(6 // 4)
-print(6. // 4)
-
-print(-6 // 4)
-print(6. // -4)
-
-print(14 % 4)
-print(12 % 4.5)
-
-print(-4 + 4)
-print(-4. + 8)
-
-print(-4 - 4)
-print(4. - 8)
-print(-1.1)
-print(+2)
-
-print(9 % 6 % 2)
-print(2 ** 2 ** 3)
-
-print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
-
-var = 1
-print(var)
-account_balance = 1000.0
-client_name = 'John Doe'
-print(var, account_balance, client_name)
-
-var = '3.8.5'
-print('Python version: ' + var)
-
-var = 1
-print(var)
-var = var + 1
-print(var)
-
-var = 100
-var = 200 + 300
-print(var)
-
-a = 3.0
-b = 4.0
-c = (a**2 + b**2)**0.5
-print('c =', c)
-
-john = 3
-mary = 5
-adam = 6
-print(john,mary,adam, sep=",")
-total_apples = john + mary + adam
-print('Total number of apples:', total_apples)
-
-kilometers = 12.25
-miles = 7.38
-
-miles_to_kilometers = miles * 1.61
-kilometers_to_miles = kilometers / 1.61
-
-print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
-print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
-
-x = -1
-x = float(x)
-y = 3*x**3 - 2*x**2 + 3*x -1
-print("y =", y)
-
-var = 2
-print(var)
-var = 3
-print(var)
-var += 1
-print(var)
-
-var = "007"
-print("Agent " + var)
+# print('Hello, World!')
+#
+# print('Hello, Python!')
+# print('Vladimir')
+#
+# print("The itsy bitsy spider climbed up the waterspout.")
+# print("Down came the rain and washed the spider out.")
+#
+# print("The itsy bitsy spider climbed up the waterspout.")
+# print()
+# print("Down came the rain and washed the spider out.")
+#
+# print("The itsy bitsy spider\nclimbed up the waterspout.")
+# print()
+# print("Down came the rain\nand washed the spider out.")
+#
+# print('\\')
+#
+# print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+#
+# print("My name is", "Python.")
+# print("Monty Python.")
+#
+# print("My name is", "Python.", end=" ")
+# print("Monty Python.")
+#
+# print("My name is ", end="")
+# print("Monty Python.")
+#
+# print("My", "name", "is", "Monty", "Python.", sep="-")
+#
+# print("My", "name", "is", sep="_", end="*")
+# print("Monty", "Python.", sep="*", end="*\n")
+#
+# print("Programming","Essentials","in", sep="***", end="...")
+# print("Python")
+#
+# print("2")
+# print(2)
+#
+# print(0o123)
+# print(0x123)
+#
+# print('I\'m Monty Python.')
+# print("I'm Monty Python.")
+#
+# print(True > False)
+# print(True < False)
+#
+# print('"I\'m"\n""learning""\n"""Python"""')
+#
+# print(2+2)
+# print(2 * 3)
+# print(2 * 3.)
+# print(2. * 3)
+# print(2. * 3.)
+#
+# print(6 / 3)
+# print(6 / 3.)
+# print(6. / 3)
+# print(6. / 3.)
+#
+# print(6 // 3)
+# print(6 // 3.)
+# print(6. // 3)
+# print(6. // 3.)
+#
+# print(6 // 4)
+# print(6. // 4)
+#
+# print(-6 // 4)
+# print(6. // -4)
+#
+# print(14 % 4)
+# print(12 % 4.5)
+#
+# print(-4 + 4)
+# print(-4. + 8)
+#
+# print(-4 - 4)
+# print(4. - 8)
+# print(-1.1)
+# print(+2)
+#
+# print(9 % 6 % 2)
+# print(2 ** 2 ** 3)
+#
+# print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+#
+# var = 1
+# print(var)
+# account_balance = 1000.0
+# client_name = 'John Doe'
+# print(var, account_balance, client_name)
+#
+# var = '3.8.5'
+# print('Python version: ' + var)
+#
+# var = 1
+# print(var)
+# var = var + 1
+# print(var)
+#
+# var = 100
+# var = 200 + 300
+# print(var)
+#
+# a = 3.0
+# b = 4.0
+# c = (a**2 + b**2)**0.5
+# print('c =', c)
+#
+# john = 3
+# mary = 5
+# adam = 6
+# print(john,mary,adam, sep=",")
+# total_apples = john + mary + adam
+# print('Total number of apples:', total_apples)
+#
+# kilometers = 12.25
+# miles = 7.38
+#
+# miles_to_kilometers = miles * 1.61
+# kilometers_to_miles = kilometers / 1.61
+#
+# print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+# print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+#
+# x = -1
+# x = float(x)
+# y = 3*x**3 - 2*x**2 + 3*x -1
+# print("y =", y)
+#
+# var = 2
+# print(var)
+# var = 3
+# print(var)
+# var += 1
+# print(var)
+#
+# var = "007"
+# print("Agent " + var)
 
 # This program evaluates the hypotenuse c.
 # a and b are the lengths of the legs.
-a = 3.0
-b = 4.0
-c = (a ** 2 + b ** 2) ** 0.5  # We use ** instead of square root.
-print("c =", c)
+# a = 3.0
+# b = 4.0
+# c = (a ** 2 + b ** 2) ** 0.5  # We use ** instead of square root.
+# print("c =", c)
 
 # anything = input("Tell me anything...")
 # print("Hmm...", anything, "...Really?")
@@ -166,9 +166,9 @@ print("c =", c)
 # print("Thank you.")
 # print("\nYour name is " + fnam + " " + lnam + ".")
 
-print("+" + 10 * "-" + "+")
-print(("|" + " " * 10 + "|\n") * 5, end="")
-print("+" + 10 * "-" + "+")
+# print("+" + 10 * "-" + "+")
+# print(("|" + " " * 10 + "|\n") * 5, end="")
+# print("+" + 10 * "-" + "+")
 
 # first_number = float(input('Please write first float number: '))
 # second_number = float(input('Please write second float number: '))
@@ -178,15 +178,15 @@ print("+" + 10 * "-" + "+")
 # div_result = first_number / second_number
 # print(f"{sum_result}\n{sub_result}\n{mul_result}\n{div_result}\nThat's all, folks")
 
-var = 0  # Assigning 0 to var
-print(var == 0)
-var = 1  # Assigning 1 to var
-print(var == 0)
-
-var = 0  # Assigning 0 to var
-print(var != 0)
-var = 1  # Assigning 1 to var
-print(var != 0)
+# var = 0  # Assigning 0 to var
+# print(var == 0)
+# var = 1  # Assigning 1 to var
+# print(var == 0)
+#
+# var = 0  # Assigning 0 to var
+# print(var != 0)
+# var = 1  # Assigning 1 to var
+# print(var != 0)
 
 # n = float(input('Enter the number: '))
 # less_then = n > 100
@@ -341,20 +341,20 @@ print(var != 0)
 # print('Ready or not, here I come!')
 
 # break - example
-print("The break instruction:")
-for i in range(1, 6):
-    if i == 3:
-        break
-    print("Inside the loop.", i)
-print("Outside the loop.")
-
-# continue - example
-print("\nThe continue instruction:")
-for i in range(1, 6):
-    if i == 3:
-        continue
-    print("Inside the loop.", i)
-print("Outside the loop.")
+# print("The break instruction:")
+# for i in range(1, 6):
+#     if i == 3:
+#         break
+#     print("Inside the loop.", i)
+# print("Outside the loop.")
+#
+# # continue - example
+# print("\nThe continue instruction:")
+# for i in range(1, 6):
+#     if i == 3:
+#         continue
+#     print("Inside the loop.", i)
+# print("Outside the loop.")
 
 # largest_number = -99999999
 # counter = 0
@@ -406,20 +406,73 @@ print("Outside the loop.")
 #     word_without_vowels += l
 # print(word_without_vowels)
 
-i = 1
-while i < 5:
-    print(i)
-    i += 1
-else:
-    print("else:", i)
+# i = 1
+# while i < 5:
+#     print(i)
+#     i += 1
+# else:
+#     print("else:", i)
+#
+# i = 111
+# for i in range(2, 1):
+#     print(i)
+# else:
+#     print("else:", i)
+#
+# for i in range(5):
+#     print(i)
+# else:
+#     print("else:", i)
 
-i = 111
-for i in range(2, 1):
-    print(i)
-else:
-    print("else:", i)
+# blocks = int(input('Enter the number of blocks: '))
+# pyramid_height = 0
+# numbers_of_used_blocks = 0
+# while blocks > 0:
+#     if pyramid_height + 1 <= blocks:
+#         pyramid_height += 1
+#         numbers_of_used_blocks += pyramid_height
+#         blocks -= pyramid_height
+#     else:
+#         break
+# print(f'The height of the pyramid: {pyramid_height}')
 
-for i in range(5):
-    print(i)
-else:
-    print("else:", i)
+# number = int(input('Enter non-negative and non-zero number: '))
+# steps_count = 0
+# while not number == 1:
+#     if number % 2 == 0:
+#         number /= 2
+#         steps_count += 1
+#         print(int(number))
+#     else:
+#         number = 3 * number + 1
+#         steps_count += 1
+#         print(int(number))
+# print(f'steps = {steps_count}')
+
+# for i in range(0, 11):
+#     if i % 2 == 1:
+#         print(i)
+
+# x = 1
+# while x < 11:
+#     if x % 2 == 1:
+#         print(x)
+#     x += 1
+
+# str_ch = ''
+# for ch in "john.smith@pythoninstitute.org":
+#     if ch == "@":
+#         break
+#     else:
+#         str_ch += ch
+# print(str_ch)
+
+# for ch in "john.smith@pythoninstitute.org":
+#     if ch == "@":
+#         break
+#     print(ch, end="")
+
+# for digit in "0165031806510":
+#     if digit == "0":
+#         digit = 'x'
+#     print(digit, end='')
