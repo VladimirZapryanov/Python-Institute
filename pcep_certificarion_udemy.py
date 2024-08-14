a9398b9e99a61b7ce07e25fb3da13a9f9aee2ef2
Vladimir Zapryanov <90334985+VladimirZapryanov@users.noreply.github.com>
2024-08-14 17:21:45 +0300

    add new exercise

--- pcep_certificarion_udemy.py
+++ pcep_certificarion_udemy.py
@@ -1,7 +1,31 @@
-# This is first code in this course
-# Day one
-print('I love Python')
-"""Printing hello world
-Writing comments here"""
-print('Hello World!!!')
-print('Test Repo')
+# # This is first code in this course
+# # Day one
+# print('I love Python')
+# """Printing hello world
+# Writing comments here"""
+# print('Hello World!!!')
+# print('Test Repo')
+
+# name = 'Python'
+# print('Hello world ' + name)
+
+# #Creating variables
+# total = 100
+# print(total)
+# sum = total
+# print(sum)
+# a = total + 10
+# print(a)
+# total = 200
+# print(total)
+# a = b = c = 500
+# print(a)
+# print(b)
+# print(c)
+# a, b, c = 1, 2, 3
+# print(a)
+# print(b)
+# print(c)
+# del total
+# print(total) # This will raise error
+
