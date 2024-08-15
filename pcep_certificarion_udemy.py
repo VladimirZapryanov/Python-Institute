50563cf7bbff216cc3573794d139eba2182901d8
Vladimir Zapryanov <90334985+VladimirZapryanov@users.noreply.github.com>
2024-08-15 17:17:14 +0300

    add new exercise

--- pcep_certificarion_udemy.py
+++ pcep_certificarion_udemy.py
@@ -29,3 +29,98 @@
 # del total
 # print(total) # This will raise error
 
+# # String Literals
+# name = 'Python'
+# print(name)
+# name_two = "Python"
+# print(name_two)
+# name_three = """I love Python"""
+# print(name_three)
+# name_three = """I
+# love
+# Python"""
+# print(name_three)
+
+# # Boolean Literals
+# b = True
+# print(b)
+# c = False
+# print(c)
+
+# # Special Literal
+# a = 10
+# print(a)
+# a = None
+# print(a)
+# del a
+
+# # Number Data Type
+# a = 10
+# print(type(a))
+# b = 2.5
+# print(type(b))
+# print(2e7)
+# c = 10 + 11j
+# print(type(c))
+
+# # Print sep and end
+# a = 10
+# b = 20
+# c = 30
+# print(a, b, c)
+# print(a, b, c, sep='')
+# print(a, b, c, sep='|')
+# print(a, b, c, sep='\n')
+# print('I love Python')
+# print('Yes')
+# print('I love Python', end='')
+# print('Yes')
+# print('I love Python', end='.')
+# print('Yes')
+# print('Mondey', 'Tuesday', 'Wednesday')
+# print('Mondey', 'Tuesday', 'Wednesday', sep=',', end=',')
+# print('Thursday', 'Friday', 'Saturday', 'Sunday', sep=',')
+
+# # Fundamental datatypes and immutability
+# a = 10
+# b = 10
+# print(id(a))
+# print(id(b))
+# b = 20
+# print(id(b))
+# c = 30
+# print(id(c))
+# print(id(a + b))
+
+# # Arithmetic operators
+# a = 10
+# b = 4
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(8 % 4)
+# print(a // b)
+# print(-a // b)
+# print(10.0 // 4)
+# print(a ** b)
+# print(2 ** 3)
+
+# Assignment operators
+a = 10
+print(a)
+a += 5
+print(a)
+a -= 5
+print(a)
+a *= 2
+print(a)
+a /= 2
+print(a)
+a %= 3
+print(a)
+a **= 2
+print(a)
+a //= 3
+print(a)
