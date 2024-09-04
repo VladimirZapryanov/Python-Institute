7f99c72a756dcfa25459a1b809d21ca5ca2ca04e
Vladimir Zapryanov <90334985+VladimirZapryanov@users.noreply.github.com>
2024-09-04 13:24:55 +0300

    add new exercise

--- pcep_certificarion_udemy.py
+++ pcep_certificarion_udemy.py
@@ -225,4 +225,138 @@
 # b = s1 * 2
 # print(b)
 
+# a = 10 + 2 * 4
+# print(a)
+# x = 10 / 5 * 2
+# print(x)
+# def get_value(i):
+#     print(i)
+#     return i
+# if __name__ == '__main__':
+#     print(get_value(10) + get_value(20) + get_value(30))
+# print(10 + 2 * 5)
+# print((10 + 2) * 5)
+# a = 10
+# b = 20
+# c = 30
+# d = 40
+# e = (a + b * c / d )
+# print(e)
+# e = ((a + b) * c) / d
+# print(e)
+# e = (a + b) * (c / d)
+# print(e)
 
+# age = 18
+# if age > 18:
+#     print("Welcome!")
+# else:
+#     print("You are not above 18. Sorry")
+
+# first_number = int(input("Enter first number: "))
+# second_number = int(input("Enter second number: "))
+# number = 0
+# if first_number > second_number:
+#     number = first_number
+# else:
+#     number = second_number
+# print(f'{number} is bigger!')
+
+# a = 0
+# if a > 0:
+#     print('Positive')
+# elif a == 0:
+#     print('Zero')
+# else:
+#     print('Negative')
+
+# age = 61
+# if age > 18:
+#     if age > 60:
+#         print('Senior citizen')
+#     else:
+#         print('Savings account')
+# else:
+#     print('Not eligible')
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# num = 0
+# if a > b and a > c:
+#     num = a
+# elif b > c:
+#     num = b
+# else:
+#     num = c
+# print('The largest number is: {}'.format(num))
+
+# a = 'I love Python'
+# for l in a:
+#     print(l)
+
+# for i in range(1, 10, 2):
+#     print(i)
+
+# for num in range(21):
+#     if num % 2 == 0:
+#         print(num)
+
+# a = 1
+# while a < 5:
+#     print(a)
+#     a += 1
+
+# a = 5
+# while a:
+#     print(a)
+#     a -= 1
+
+# num = int(input('Enter number: '))
+# num_sum = 0
+# for n in range(num + 1):
+#     num_sum += n
+# print(num_sum)
+
+# num = int(input('Enter number: '))
+# num_sum = i = 0
+# while i <= num:
+#     num_sum += i
+#     i += 1
+# print(num_sum)
+
+# a = 5
+# while a:
+#     print(a)
+#     a = a - 1
+#     if a == 2:
+#         break
+#     print('Inside while loop')
+# print('Outside while loop')
+
+# a = [1, 2, 3, 4, 5]
+# for x in a:
+#     print(x)
+#     if x == 3:
+#         break
+#     print('Inside for loop')
+# print('Outside for loop')
+
+# a = 5
+# while a:
+#     print(a)
+#     a = a - 1
+#     if a == 2:
+#         continue
+#     print('End of the while loop')
+
+# a = [1, 2, 3, 4, 5]
+# for x in a:
+#     print(x)
+#     if x == 2:
+#         continue
+#     print('End of for loop')
+
+# a = 1
+# while a < 10:
+#     pass
